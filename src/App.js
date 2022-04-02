56b0736dc0c412181c52cd9722e58ab8006772d8
Rahul Sunil Moje <rahulmoje@dal.ca>
2022-04-01 23:53:15 -0300

    Consume test endpoing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import IssueBook from "./components/issueBook/IssueBook";
 import TestConnectivity from "./pages/TestConnectivity";
+import StaticPage from "./pages/StaticPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/test" element={<TestConnectivity />} />
           <Route path="/" element={ <IssueBook />} />
+          <Route path="/static" element={<StaticPage />} />
         </Routes>
       </BrowserRouter>
     </div>
