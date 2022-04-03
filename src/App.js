0256fb0aa44304cc8d735378b6a1ad5bce040a95
Harivansh Bhatia <hr513288@dal.ca>
2022-04-02 23:44:16 -0300

    login,signup,and verify user

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import "./App.css";
 import IssueBook from "./components/issueBook/IssueBook";
 import Header from './components/Header';
+import SignUp from "./components/Login/SignUp";
+import Login from "./components/Login/Login";
+import Authentication from "./components/Login/Authentication";
 
 function App() {
-  return (
-    <div className="app">
-      <IssueBook />
-    </div>
-  );
+    return (
+        <div className="app">
+            <Header/>
+            <Routes>
+                <Route path="/" element={<SignUp/>}></Route>
+                <Route path="/login" element ={<Login/>}></Route>
+                <Route path="/issuebook" element={<IssueBook/>}></Route>
+                <Route path="/authenticate" element={<Authentication />}></Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Login/SignUp.js
@@ -0,0 +1,64 @@
+import React from "react";
+import {useState} from "react";
+import {CognitoUserAttribute, CognitoUserPool} from "amazon-cognito-identity-js";
+import {useNavigate} from 'react-router-dom';
+import poolData from "./PoolData";
+
+
+
+function SignUp(){
+    const navigate = useNavigate();
+    const[email,setEmail] = useState("");
+    const[password,setPassword] = useState("");
+    const[mobileNumber,setMobileNumber] = useState("");
+
+    // const poolData = {
+    //     UserPoolId: "us-east-1_jy0VVY4Ai",
+    //     ClientId: "15gjmj51qgejmfs4s6jjdedg67"
+    //
+    // }
+    const userPool = new CognitoUserPool(poolData);
+
+
+
+    const onSubmit = (event)=>{
+        event.preventDefault();
+        //referenced from https://www.youtube.com/watch?v=Yp5sZd7ZyCI
+        const attributeList = [
+            new CognitoUserAttribute({
+                Name: 'phone_number',
+                Value: mobileNumber,
+            }),
+        ];
+
+        navigate("/authenticate",{state:{email:email}})
+
+        userPool.signUp(email,password,attributeList,null,(err,data) =>{
+            if(err){
+                if(err.code==='UsernameExistsException'){
+                    navigate("/login")
+                }
+            }
+            else {
+                navigate("/authenticate",{state:{email}})
+            }
+        })
+    }
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <label htmlFor="email">Email</label>
+                <input value={email} onChange={(event)=> setEmail(event.target.value)}></input>
+
+                <label htmlFor="password">Password</label>
+                <input value={password} onChange={(event)=>setPassword(event.target.value)}></input>
+
+                <label htmlFor="mobileNumber">Mobile Number</label>
+                <input value={mobileNumber} onChange={(e)=>setMobileNumber(e.target.value)}></input>
+
+                <button type="submit">Signup</button>
+            </form>
+        </div>
+    )
+}
+export default SignUp;
